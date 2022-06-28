4245a34964bf2cba359417b9884d7987e3363f6a
theProgrammingBox <justinboxgea@gmail.com>
2022-06-28 15:16:09 -0700

    averageing forces may be the problem

--- Self Attempted Physics/Ball.h
+++ Self Attempted Physics/Ball.h
@@ -18,23 +18,24 @@ public:
 	float inverseMass;
 	float inverseInertia;
 	vf2d normal;
-	/*vf2d totalForce;
+	vf2d totalForce;
+	float totaTorque;
 	int numForces;
-	float torque;
-	int numTorques;*/
-	vector<vf2d> forces;
-	vector<float> torques;/**/
+	int numTorques;
 	vf2d acceleration;
 	float angularAcceleration;
 
-	Ball() : position(0, 0), velocity(0, 0), angle(0), angularVelocity(0), radius(10), density(1), elasticity(1), friction(1), color(WHITE),
+	Ball() :
+		position(0, 0), velocity(0, 0), angle(0), angularVelocity(0), radius(10), density(1), elasticity(1), friction(1), color(WHITE),
 		inverseMass(1 / (density * PI * radius * radius)), inverseInertia(2.0f * inverseMass / (radius * radius)), normal(cos(angle), sin(angle)),
-		forces(0), torques(0), acceleration(0, 0), angularAcceleration(0) {};
+		totalForce(0, 0), totaTorque(0), numForces(0), numTorques(0),
+		acceleration(0, 0), angularAcceleration(0) {};
 
 	Ball(vf2d position, vf2d velocity, float angle, float angularVelocity, float radius, float density, float elasticity, float friction, Pixel color) :
 		position(position), velocity(velocity), angle(angle), angularVelocity(angularVelocity), radius(radius), density(density), elasticity(elasticity), friction(friction), color(color),
 		inverseMass(1 / (density * PI * radius * radius)), inverseInertia(2.0f * inverseMass / (radius * radius)), normal(cos(angle), sin(angle)),
-		forces(0), torques(0), acceleration(0, 0), angularAcceleration(0) {};
+		totalForce(0, 0), totaTorque(0), numForces(0), numTorques(0),
+		acceleration(0, 0), angularAcceleration(0) {};
 
 	void SetPosition(vf2d position) { this->position = position; }
 	void SetVelocity(vf2d velocity) { this->velocity = velocity; }
@@ -47,13 +48,39 @@ public:
 	void SetColor(Pixel color) { this->color = color; }
 	void SetInverseMass(float inverseMass) { this->inverseMass = inverseMass; this->inverseInertia = 2.0f * inverseMass / (radius * radius); }
 
-	void AddForce(vf2d force) { forces.push_back(force); }
-	void AddTorque(float torque) { torques.push_back(torque); }
+	void AddForce(vf2d force) { totalForce += force; numForces++; }
+	void AddTorque(float torque) { totaTorque += torque;  numTorques++; }
 	void AddAcceleration(vf2d acceleration) { this->acceleration += acceleration; }
 	void AddAngularAcceleration(float angularAcceleration) { this->angularAcceleration += angularAcceleration; }
 
-	void ApplyForces();
-	void ApplyAccelerations(float dt);
+	void ApplyForces()
+	{
+		if (numForces != 0)
+		{
+			velocity += totalForce / numForces * inverseMass;
+			totalForce = vf2d(0, 0);
+			numForces = 0;
+		}
+		if (numTorques != 0)
+		{
+			angularVelocity += totaTorque / numTorques * inverseInertia;
+			totaTorque = 0;
+			numTorques = 0;
+		}
+	}
 
-	void Update(float dt);
+	void ApplyAccelerations(float dt)
+	{
+		velocity += acceleration * dt;
+		angularVelocity += angularAcceleration * dt;
+		acceleration = vf2d(0, 0);
+		angularAcceleration = 0;
+	}
+
+	void Update(float dt)
+	{
+		position += velocity * dt;
+		angle += angularVelocity * dt;
+		normal = vf2d(cos(angle), sin(angle));
+	}
 };
